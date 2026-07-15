9e2372e989c6f8da02ff5d791481e76461f30c94
DURNUTALA SAI TEJASWINI <saitejaswini0813@gmail.com>
2026-07-15 11:56:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -729,5 +729,6 @@ def admin_logout():
 
     return redirect("/admin_login")
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
    
